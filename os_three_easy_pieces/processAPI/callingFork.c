53348b5914e2928a299d56a59b27319447eda839
kaushikmak <kaushikmak35@gmail.com>
2026-06-25 12:35:35 +0530

    how to call fork

--- os_three_easy_pieces/processAPI/callingFork.c
+++ os_three_easy_pieces/processAPI/callingFork.c
@@ -4,14 +4,14 @@
 
 int main(){
     printf("hello world (pid:%d)\n",(int)getpid());
-    int rc = fork();
-    if(rc < 0){
+    int pid = fork();
+    if(pid < 0){
         fprintf(stderr, "fork failed\n");
         exit(1);
-    }else if(rc == 0){
+    }else if(pid == 0){
         printf("hello, this is child process (pid:%d)\n",(int)getpid());
     }else{
-        printf("hello this is parent of %d (pid:%d)\n",rc,(int)getpid());
+        printf("hello this is parent of %d (pid:%d)\n",pid,(int)getpid());
     }
     return 0;
 }
